56931061e6cca28b2fbb5ac5653fee1cf758cde7
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-04 11:27:40 -0400

    Setting up file structure

---
+++ foodorderapp/src/components/header/Header.js
@@ -0,0 +1,20 @@
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import './Header.css';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className='header'>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className='main-image'>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </>
+  );
+};
+
+export default Header;
+
