84b92fb8d06da2bced462d3cccbca68f6c10870a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-05 13:44:45 -0400

    Using the useReducer hook to be able to add items and then calculate the total

--- foodorderapp/src/store/CartProvider.js
+++ foodorderapp/src/store/CartProvider.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cart-context'
 
-const CartProvider = ({ children }) => {
-  const AddItemToCart = (item) => {
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
 
+const cartReducer = (state, action) => {
+  if (action.type === 'Add Item') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotal = state.totalAmount + action.item.price * action.item.amount
+    return { item: updatedItems, totalAmount: updatedTotal }
+  } else if (action.type === 'Remove Item') {
+
+  }
+  return defaultCartState
+}
+
+const CartProvider = ({ children }) => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState)
+  const AddItemToCart = (item) => {
+    dispatchCart({ type: 'Add Item', item: item })
   }
 
   const RemoveItemFromCart = (id) => {
-
+    dispatchCart({ type: 'Remove Item', id: id })
   }
 
   const Context = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: AddItemToCart,
     removeItem: RemoveItemFromCart
   }
