4cd145225a4559ca3ff5a0d898b0d45c4d3219a3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-16 16:23:54 -0500

    Added error handling if the input isnt correct

--- foodorderapp/src/components/checkout/Checkout.js
+++ foodorderapp/src/components/checkout/Checkout.js
@@ -1,10 +1,50 @@
 import './Checkout.css'
 
+import { useRef, useState } from 'react'
+
+const isEmpty = (value => value.trim().length === 0)
+const is5Chars = (value => value.trim().length === 5)
 
 const Checkout = ({ onClose }) => {
 
+  const [name, setName] = useState(true)
+  const [address, setAddress] = useState(true)
+  const [zipcode, setZipCode] = useState(true)
+  const [city, setCity] = useState(true)
+
+
+  const nameInput = useRef()
+  const addressInput = useRef()
+  const zipcodeInput = useRef()
+  const cityInput = useRef()
+
+
   const handleConfirm = (e) => {
     e.preventDefault()
+
+    const enteredname = nameInput.current.value
+    const enteredaddress = addressInput.current.value
+    const enteredzip = zipcodeInput.current.value
+    const enteredcity = cityInput.current.value
+
+    const enterednameIsValid = !isEmpty(enteredname)
+    const enteredaddressIsValid = !isEmpty(enteredaddress)
+    const enteredcityIsValid = !isEmpty(enteredcity)
+    const enteredzipIsValid = is5Chars(enteredzip)
+
+    setName(enterednameIsValid)
+    setAddress(enteredaddressIsValid)
+    setZipCode(enteredzipIsValid)
+    setCity(enteredcityIsValid)
+
+    const formIsValid = enterednameIsValid && enteredaddressIsValid &&
+      enteredcityIsValid && enteredzipIsValid
+
+
+
+    if (!formIsValid) {
+      return
+    }
   }
 
   return (
@@ -12,19 +52,23 @@ const Checkout = ({ onClose }) => {
       <form className='form' onSubmit={handleConfirm}>
         <div className='control'>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' ref={nameInput} />
+          {!name && <p>Please enter a valid name</p>}
         </div>
         <div className='control'>
           <label htmlFor='address'>Address</label>
-          <input type='text' id='address' />
+          <input type='text' id='address' ref={addressInput} />
+          {!address && <p>Please enter a valid Address</p>}
         </div>
         <div className='control'>
           <label htmlFor='zipcode'>Zip Code</label>
-          <input type='text' id='zipcode' />
+          <input type='text' id='zipcode' ref={zipcodeInput} />
+          {!zipcode && <p>Please enter a valid Zip Code</p>}
         </div>
         <div className='control'>
           <label htmlFor='city'>City</label>
-          <input type='text' id='city' />
+          <input type='text' id='city' ref={cityInput} />
+          {!city && <p>Please enter a valid City</p>}
         </div>
         <div className='actions'>
           <button className='submit' type='submit'>Confirm</button>
