b89bd9e4f54264f84a97293c83fa2e1d8b8e628a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-16 10:28:29 -0500

    Using the fetch call to get data from firebase

--- foodorderapp/src/components/avaliablemeals/AvaliableMeals.js
+++ foodorderapp/src/components/avaliablemeals/AvaliableMeals.js
@@ -1,52 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './AvaliableMeals.css'
 import Card from '../card/Card';
 import MealItem from '../mealitem/MealItem';
 
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
-
 const AvaliableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <MealItem
-    key={meal.id}
-    id={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price} />)
+  const [meals, setMeals] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        setLoading(true)
+        const response = await fetch('https://react-foodapp-ac153-default-rtdb.firebaseio.com/meals.json')
+        const data = await response.json()
+        const loadedMeals = []
+
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price
+          })
+        }
+        setMeals(loadedMeals)
+      } catch (err) {
+        return err
+      }
+    }
+    fetchMeals()
+
+  }, [])
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+
+  ))
+
   return (
-    <section className='meals'>
-      <ul>
-        <Card>
-          {mealsList}
-        </Card>
-      </ul>
-    </section>
+    <>
+      <section className='meals'>
+        <ul>
+          <Card>
+            {mealsList}
+          </Card>
+        </ul>
+      </section>
+    </>
   )
 }
 
