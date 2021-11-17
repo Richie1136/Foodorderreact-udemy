92d6be88d1f5658e74d4923e9e4371a9e70dc9bd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-17 09:58:02 -0500

    Used what i did in the last section which was all about form validation to help me get correct form validation for this form with 4 inputs and have the correct handling

--- foodorderapp/src/components/checkout/Checkout.js
+++ foodorderapp/src/components/checkout/Checkout.js
@@ -2,15 +2,20 @@ import './Checkout.css'
 
 import { useRef, useState } from 'react'
 
+
 const isEmpty = (value => value.trim().length === 0)
 const is5Chars = (value => value.trim().length === 5)
 
 const Checkout = ({ onClose }) => {
 
-  const [name, setName] = useState(true)
-  const [address, setAddress] = useState(true)
-  const [zipcode, setZipCode] = useState(true)
-  const [city, setCity] = useState(true)
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+  const [zipcode, setZipCode] = useState('')
+  const [city, setCity] = useState('')
+  const [nametouched, setNameTouched] = useState(false)
+  const [addresstouched, setAddressTouched] = useState(false)
+  const [ziptouched, setZipTouched] = useState(false)
+  const [citytouched, setCityTouched] = useState(false)
 
 
   const nameInput = useRef()
@@ -21,6 +26,10 @@ const Checkout = ({ onClose }) => {
 
   const handleConfirm = (e) => {
     e.preventDefault()
+    setNameTouched(true)
+    setAddressTouched(true)
+    setZipTouched(true)
+    setCityTouched(true)
 
     const enteredname = nameInput.current.value
     const enteredaddress = addressInput.current.value
@@ -32,18 +41,19 @@ const Checkout = ({ onClose }) => {
     const enteredcityIsValid = !isEmpty(enteredcity)
     const enteredzipIsValid = is5Chars(enteredzip)
 
-    const enterednameIsInValid = !enterednameIsValid
-    const nameClass = enterednameIsInValid ? 'control invalid' : 'control'
 
-    setName(enterednameIsValid)
-    setAddress(enteredaddressIsValid)
-    setZipCode(enteredzipIsValid)
-    setCity(enteredcityIsValid)
+    setName('')
+    setAddress('')
+    setZipCode('')
+    setCity('')
+    setNameTouched(false)
+    setAddressTouched(false)
+    setCityTouched(false)
+    setZipTouched(false)
+
 
     const formIsValid = enterednameIsValid && enteredaddressIsValid &&
       enteredcityIsValid && enteredzipIsValid
-
-
 
     if (!formIsValid) {
       return
@@ -51,28 +61,57 @@ const Checkout = ({ onClose }) => {
   }
 
 
+  const onNameBlur = (e) => {
+    setNameTouched(false)
+  }
+
+  const onNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const onAddressBlur = (e) => {
+    setAddressTouched(false)
+  }
+
+  const onAddressChange = (e) => {
+    setAddress(e.target.value)
+  }
+  const onZipBlur = (e) => {
+    setZipTouched(false)
+  }
+
+  const onZipChange = (e) => {
+    setZipCode(e.target.value)
+  }
+  const onCityBlur = (e) => {
+    setCityTouched(false)
+  }
+
+  const onCityChange = (e) => {
+    setCity(e.target.value)
+  }
 
   return (
     <div>
       <form className='form' onSubmit={handleConfirm}>
         <div className={`control ${name ? '' : 'invalid'}`}>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' ref={nameInput} />
+          <input type='text' id='name' ref={nameInput} onChange={onNameChange} onBlur={onNameBlur} value={name} />
           {!name && <p>Please enter a valid name</p>}
         </div>
         <div className={`control ${address ? '' : 'invalid'}`}>
           <label htmlFor='address'>Address</label>
-          <input type='text' id='address' ref={addressInput} />
+          <input type='text' id='address' ref={addressInput} onChange={onAddressChange} onBlur={onAddressBlur} value={address} />
           {!address && <p>Please enter a valid Address</p>}
         </div>
         <div className={`control ${zipcode ? '' : 'invalid'}`}>
           <label htmlFor='zipcode'>Zip Code</label>
-          <input type='text' id='zipcode' ref={zipcodeInput} />
+          <input type='text' id='zipcode' ref={zipcodeInput} onChange={onZipChange} onBlur={onZipBlur} value={zipcode} />
           {!zipcode && <p>Please enter a valid Zip Code</p>}
         </div>
         <div className={`control ${city ? '' : 'invalid'}`}>
           <label htmlFor='city'>City</label>
-          <input type='text' id='city' ref={cityInput} />
+          <input type='text' id='city' ref={cityInput} onChange={onCityChange} onBlur={onCityBlur} value={city} />
           {!city && <p>Please enter a valid City</p>}
         </div>
         <div className='actions'>
@@ -82,7 +121,6 @@ const Checkout = ({ onClose }) => {
       </form>
     </div>
   )
-
 }
 
 export default Checkout
